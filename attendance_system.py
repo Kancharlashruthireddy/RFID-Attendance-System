f2445446d8962c2f0341596302467bd8e621683b
Kancharlashruthireddy <shruthireddykancharla2027@gmail.com>
2026-06-17 14:56:49 +0530

    Refactor attendance system to read from CSV file

--- attendance_system.py
+++ attendance_system.py
@@ -1,14 +1,35 @@
-students = {
-    "101": "Ravi",
-    "102": "Priya",
-    "103": "Kiran",
-    "104": "Anjali"
-}
+import csv
+from datetime import datetime
+
+students = {}
+
+with open("students.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students[row["RFID_ID"]] = {
+            "name": row["Name"],
+            "roll": row["Roll_No"]
+        }
 
 card_id = input("Scan RFID Card: ")
 
 if card_id in students:
-    print("Attendance Marked")
-    print("Student:", students[card_id])
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open("attendance.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow([
+            students[card_id]["roll"],
+            students[card_id]["name"],
+            current_time
+        ])
+
+    print("\nAttendance Marked Successfully")
+    print("Name :", students[card_id]["name"])
+    print("Roll :", students[card_id]["roll"])
+    print("Time :", current_time)
+
 else:
-    print("Invalid Card")
+    print("Invalid RFID Card")
